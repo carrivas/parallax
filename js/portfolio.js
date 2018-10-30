d6d8b2a4c9e36b751806d53844b7f065596f19e9
Carlos Arrivas <carlos.arrivas.ui@gmail.com>
2018-10-30 00:48:50 -0700

    smooth scroll

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,39 @@
-// var position = $("#image").offset().top;
+$(document).ready(function(){
+    
+    $(document).on('click','.nav-links', function(e){
+        e.preventDefault();
+        var linkToAttr = $(this).attr('data-link-to');
+        
+        window.scroll({
+            top: $(linkToAttr).offset().top, 
+            left: 0, 
+            behavior: 'smooth' 
+        });
+        
+        this.blur();
+    });
 
-
+});
 // $(function() { $("#about").on('click', function() { 
 //     var position = $(".home-about-textbox").offset().top;
 
 //     $(this).animate({ 
-//             scroll: position 
+//             scrollTop: position 
 //     }, 1000); 
 //     }); 
 // });
+// $("#about").click(function(e){       
+//     e.preventDefault();
+//     $('html,body').animate({scrollTop:$(".home-about-textbox").offset().top}, "slow");
+// });
 
 // $(document).ready(function(){
+//     $("#about").click(function(){
+//         var position = $(".home-about-textbox").offset().top;
+//         $("body").animate({
+//             scrollTop: position;
+//         });
+//     });
 //     $(document).on('click', '#about', function(){
 //         var position = $(".home-about-textbox").offset().top;
     
