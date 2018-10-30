e8d423fcc6784bb0f2888291e1d0a789fb3ef4bc
Carlos Arrivas <carlos.arrivas.ui@gmail.com>
2018-10-30 15:31:46 -0700

    modals, sticky nav

--- js/portfolio.js
+++ js/portfolio.js
@@ -14,61 +14,10 @@ $(document).ready(function(){
     });
 
 });
-// $(function() { $("#about").on('click', function() { 
-//     var position = $(".home-about-textbox").offset().top;
 
-//     $(this).animate({ 
-//             scrollTop: position 
-//     }, 1000); 
-//     }); 
-// });
-// $("#about").click(function(e){       
-//     e.preventDefault();
-//     $('html,body').animate({scrollTop:$(".home-about-textbox").offset().top}, "slow");
-// });
-
-// $(document).ready(function(){
-//     $("#about").click(function(){
-//         var position = $(".home-about-textbox").offset().top;
-//         $("body").animate({
-//             scrollTop: position;
-//         });
-//     });
-//     $(document).on('click', '#about', function(){
-//         var position = $(".home-about-textbox").offset().top;
-    
-//         $(this).animate({
-//             scroll: position
-//         }, 1000) 
-//     });
-// });
-
-
-// $(document).ready(function(){
-
-//     var scrollLink = $('.scroll');
-
-//     // Smooth scrolling
-// scrollLink.click(function(e) {
-//     e.preventDefault();
-//     $('body,html').animate({
-//         scrollTop: $(this.hash).offset().top
-//     }, 1000)
-//     })
-
-    // //Active status
-    // $(window).scroll(function() {
-    //     var scrollbarLocation = $(this).scrollTop();
-
-    //     scrollLink.each(function() {
-
-    //         var sectionOffset = $(this.hash).offset()top
-
-    //         if ( sectionOffset <=scrollbarLocation ) {
-    //             $(this).parent().addClass('active');
-    //             $(this).parent().siblings().removeClass('active');
-    //         }
-    //     })
-    // })
-//  })
-
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar-fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
