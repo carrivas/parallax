e9e220700b9263fd941e325f18691961b3a2f0aa
Carlos Arrivas <carlos.arrivas.ui@gmail.com>
2018-11-03 12:53:11 -0700

    formbucket

--- js/form.js
+++ js/form.js
@@ -1,24 +1,40 @@
 // YOUR CODE HERE
 // Event listener for form submission
-$("#sendEmail").click( function(){
-
-  // Save the first name as a variable
-var firstName = $("#firstName").val();
-var email = $("#email").val();
-var message = $("#message").val();
-
-  // Clear all fields in form
-$(".form-control").val("");
-
-// if the user enters first name, email, and message
-// show message has been sent
-// if any input is missing
-// show inputs needed to complete
-if (firstName) {
-    // Set HTML of .modal-body
-$(".modal-body").html("Thank you, " + firstName + ".  Your message has been sent!");
-}
-else {
-$(".modal-body").html("Sorry, your form is not complete!");
-}
+$("#sendEmail").click(function(){
+  submitForm();
 });
+//   // Save the first name as a variable
+// var firstName = $("#firstName").val();
+// var email = $("#email").val();
+// var message = $("#message").val();
+
+//   // Clear all fields in form
+// $(".form-control").val("");
+
+// // if the user enters first name, email, and message
+// // show message has been sent
+// // if any input is missing
+// // show inputs needed to complete
+// if (firstName) {
+//     // Set HTML of .modal-body
+// $(".modal-body").html("Thank you, " + firstName + ".  Your message has been sent!");
+// }
+// else {
+// $(".modal-body").html("Sorry, your form is not complete!");
+// }
+// });
+
+function submitForm() {
+  var formData = $("#contact-form").serialize();
+  // console.log( $('#test').val() );
+  console.log( formData );
+  $.post("https://api.formbucket.com/f/buk_FUb5LCfdQtWdd8vYVeuJ6I5s", formData).done(function(data){
+      // if everything is good
+      if(data.id){
+        alert('Message Sent.  Thanks!');
+        //if something is wrong
+      }else{
+        alert('Message did not send.  Try again!');
+      }
+  });
+}
