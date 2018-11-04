3ba255df382f6aa2258ab7182a9e769916a73005
Carlos Arrivas <carlos.arrivas.ui@gmail.com>
2018-11-03 22:04:06 -0800

    projects

--- js/parallax.js
+++ js/parallax.js
@@ -6,7 +6,7 @@ function parallax() {
 
     var wScroll = $(window).scrollTop();
 
-    $('.parallax--bg').css('background-position','center '+(wScroll*0.7)+'px')
+    $('.parallax--bg').css('background-position','center '+(wScroll*0.85)+'px')
 
     // $('.parallax--box').css('top',-5-(wScroll*0.01)+'em')
 
